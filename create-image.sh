5fbce040fa99bf8683600013e7dee72552b36952
nicholaspan <nicholas.pan2@gmail.com>
2016-01-21 14:29:13 -0800

    Update create-image.sh

--- create-image.sh
+++ create-image.sh
@@ -18,7 +18,7 @@ aws ec2 create-image --instance-id $1 --name "$AMI_ID" --description "$AMI_DESCR
 
 # This will let the user know the command had a successful response code
 if [ $? -eq 0 ]; then
-	echo "AMI creation of $1 has been successful"
+        echo "AMI creation of $1 has been successful"
 fi
 
 # This will check for AMI's older than 5 days ago
@@ -27,23 +27,30 @@ echo "Checking for AMI's of instance $1 older than 5 days"
 
 aws ec2 describe-images --filters "Name=name,Values=$OLD_AMI" | grep -i imageid | cut -d'"' -f4 > /tmp/amiid.txt
 
-image_id=`cat /tmp/amiid.txt`
-lines=`wc -l /tmp/amiinfo.txt | cut -d' ' -f1`
+# Will only execute if there is an AMI older than 5 days
 
-# Will only execute if an image is found
-if [ "$lines" -gt "0" ] ; then
+if [ `wc -l /tmp/amiid.txt | cut -d' ' -f1` -gt "0" ] ; then
 
-	#This will find the snapshots associated with the image
-	aws ec2 describe-images --image-ids "$image_id" | grep -i snap | cut -d'"' -f4 > /tmp/snapids.txt
+    image_id=`cat /tmp/amiid.txt`
+    lines=`wc -l /tmp/amiinfo.txt | cut -d' ' -f1`
 
-	#This deregisters the old image
-	echo "Deregistering image $OLD_AMI"
-	aws ec2 deregister-image --image-id "$image_id"
+    # Will only execute if the image is found
+    if [ "$lines" -gt "0" ] ; then
 
-	
-	#This will remove the snapshots associated with the image
-	for i in `cat /tmp/snapids.txt`
-		do
-	aws ec2 delete-snapshot --snapshot-id $i
-	done
+            #This will find the snapshots associated with the image
+            aws ec2 describe-images --image-ids "$image_id" | grep -i snap | cut -d'"' -f4 > /tmp/snapids.txt
+
+            #This deregisters the old image
+            echo "Deregistering image $OLD_AMI"
+            aws ec2 deregister-image --image-id "$image_id"
+
+
+            #This will remove the snapshots associated with the image
+            for i in `cat /tmp/snapids.txt`
+                    do
+            aws ec2 delete-snapshot --snapshot-id $i
+            done
+    fi
+    else
+        echo "There are no current images of $1 older than 5 days"
 fi
